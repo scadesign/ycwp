04c7af5fa7a874b23686c56c61297f7131d8750e
Stephen Arscott <stephenarscott@Stephens-iMac.local>
2021-05-10 11:45:19 +0100

    more validation and work on the API controllers

--- public/data/controller/session.php
+++ public/data/controller/session.php
@@ -2,6 +2,14 @@
 require_once('db.php');
 require_once('../model/Response.php');
 
+/*
+login a volunteer and create a session for the volunteer or 
+update the session if it already exist
+GEt and Patch methods used to check and update the session
+Post used to login
+Response sent to client using the response model and json data listing errors or success
+*/
+
 try{
     $dB = DB::connectDB();
 } catch(PDOException $e) {
@@ -14,8 +22,10 @@ try{
     exit; 
 }
 
+// check a session has been created
 if(array_key_exists("sessionid", $_GET)) {
 
+    //if a sesssion exists assign its id
     $sessionId = $_GET['sessionid'];
 
     if($sessionId ==='' || !is_numeric($sessionId)) {
@@ -27,7 +37,7 @@ if(array_key_exists("sessionid", $_GET)) {
         $response->send();
         exit; 
     }
-
+    //Check an acces token exists and is not blank
     if(!isset($_SERVER['HTTP_AUTHORIZATION']) || strlen($_SERVER['HTTP_AUTHORIZATION']) <1) {
         $response = new Response();
         $response->setHttpStatusCode(401);
@@ -38,8 +48,10 @@ if(array_key_exists("sessionid", $_GET)) {
         exit; 
     }
 
+    //if access token exists assign it
     $access_token = $_SERVER['HTTP_AUTHORIZATION'];
 
+    // set up the logout for a volunteer
     if($_SERVER['REQUEST_METHOD'] === 'DELETE') {
         try {
             $query = $dB->prepare('delete from tblsessions where id = :sessionid and accesstoken = :accesstoken');
@@ -53,6 +65,7 @@ if(array_key_exists("sessionid", $_GET)) {
             $returnData['session'] = $sessionId;
             $returnData['token'] = $access_token;
 
+            // if no session matching the details was found
             if($rowCount === 0) {
                 $response = new Response();
                 $response->setHttpStatusCode(400);
@@ -62,7 +75,7 @@ if(array_key_exists("sessionid", $_GET)) {
                 $response->send();
                 exit; 
             }
-
+            // if session data found sign out
             $returnData = array();
             $returnData['session_id'] = intval($sessionId);
 
@@ -73,7 +86,7 @@ if(array_key_exists("sessionid", $_GET)) {
             $response->addMessage("Logged out");
             $response->send();
             exit; 
-
+        // database error
         }catch(PDOException $e) {
             $response = new Response();
             $response->setHttpStatusCode(500);
@@ -82,8 +95,9 @@ if(array_key_exists("sessionid", $_GET)) {
             $response->send();
             exit; 
         }
-
+    // if new access token is required
     }elseif($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+        // check to see if content type header is valid json header
         if($_SERVER['CONTENT_TYPE'] !== 'application/json'){
             $response = new Response();
             $response->setHttpStatusCode(400);
@@ -92,9 +106,10 @@ if(array_key_exists("sessionid", $_GET)) {
             $response->send();
             exit; 
         }
-
+        // assign the content
         $rawPatchData = file_get_contents('php://input');
         
+        // check the content body is valid json
         if(!$jsonData = json_decode($rawPatchData)) {
             $response = new Response();
             $response->setHttpStatusCode(400);
@@ -103,7 +118,7 @@ if(array_key_exists("sessionid", $_GET)) {
             $response->send();
             exit; 
         }
-
+         // check there is a refresh token and the refresh token is not empty
         if(!isset($jsonData->refresh_token) || strlen($jsonData->refresh_token) < 1) {
             $response = new Response();
             $response->setHttpStatusCode(400);
@@ -115,17 +130,17 @@ if(array_key_exists("sessionid", $_GET)) {
         }
 
         try {
-
+            // assign the content body and find the session
             $refreshToken = $jsonData->refresh_token;
 
-            $query =$writeDB->prepare('select tblsessions.id as sessionid, tblsessions.userid as userid, accesstoken, refreshtoken, useractive, loginattempts, accesstokenexpiry, refreshtokenexpiry from tblsessions, tblusers where tblusers.id = tblsessions.userid and tblsessions.id = :sessionid and tblsessions.accesstoken = :accesstoken and tblsessions.refreshtoken = :refreshtoken');
+            $query =$writeDB->prepare('select tblsessions.id as sessionid, tblsessions.userid as userid, accesstoken, refreshtoken, accesstokenexpiry, refreshtokenexpiry from tblsessions, tblusers where tblusers.id = tblsessions.userid and tblsessions.id = :sessionid and tblsessions.accesstoken = :accesstoken and tblsessions.refreshtoken = :refreshtoken');
             $query->bindParam(':sessionid', $sessionId, PDO::PARAM_INT);
             $query->bindParam(':accesstoken', $access_token, PDO::PARAM_STR);
             $query->bindParam(':refreshtoken', $refresh_token, PDO::PARAM_STR);
             $query->execute();
 
             $rowCount = $query->rowCount();
-
+            //if the seeion doesn't exist
             if($rowCount === 0) {
                 $response = new Response();
                 $response->setHttpStatusCode(401);
@@ -134,7 +149,7 @@ if(array_key_exists("sessionid", $_GET)) {
                 $response->send();
                 exit; 
             }
-
+        // database error
         }catch(PDOException $e) {
             $response = new Response();
             $response->setHttpStatusCode(500);
@@ -144,7 +159,7 @@ if(array_key_exists("sessionid", $_GET)) {
             exit; 
         }
 
-
+    // if patch request method not allowed
     }else{
         $response = new Response();
         $response->setHttpStatusCode(405);
@@ -156,6 +171,7 @@ if(array_key_exists("sessionid", $_GET)) {
 
 } elseif(empty($_GET)) {
 
+    // login a volunteer with post request
     if($_SERVER['REQUEST_METHOD'] !=='POST') {
         $response = new Response();
         $response->setHttpStatusCode(405);
@@ -166,7 +182,7 @@ if(array_key_exists("sessionid", $_GET)) {
     }
 
     sleep(1);
-
+    // check content type header is set to json
     if($_SERVER['CONTENT_TYPE'] !== 'application/json') {
         $response = new Response();
         $response->setHttpStatusCode(400);
@@ -175,9 +191,9 @@ if(array_key_exists("sessionid", $_GET)) {
         $response->send();
     exit;
     }
-
+    // assign the content
     $rowPostData = file_get_contents('php://input');
-
+    // check content is valid json
     if(!$jsonData = json_decode($rowPostData)) {
         $response = new Response();
         $response->setHttpStatusCode(400);
@@ -186,23 +202,24 @@ if(array_key_exists("sessionid", $_GET)) {
         $response->send();
         exit;
     }   
-
-    if(!isset($jsonData->username) || !isset($jsonData->password)) {
+    //check an email and password has been supplied
+    if(!isset($jsonData->email) || !isset($jsonData->password)) {
         $response = new Response();
         $response->setHttpStatusCode(400);
         $response->setSuccess(false);
-        (!isset($jsonData->username) ? $response->addMessage("Username not supplied") : false);
+        (!isset($jsonData->username) ? $response->addMessage("email not supplied") : false);
         (!isset($jsonData->password) ? $response->addMessage("Password not supplied") : false);
         $response->send();
         exit;
     }
 
-    if(strlen($jsonData->username) < 1 || strlen($jsonData->username) > 255 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 255) {
+    // validate the username and password content
+    if(strlen($jsonData->email) < 1 || strlen($jsonData->email) > 255 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 255) {
         $response = new Response();
         $response->setHttpStatusCode(400);
         $response->setSuccess(false);
-        (strlen($jsonData->username) < 1 ? $response->addMessage("Username cannot be blank") : false);
-        (strlen($jsonData->username) > 255 ? $response->addMessage("Username cannot be greater than 255 characters") : false);
+        (strlen($jsonData->username) < 1 ? $response->addMessage("email cannot be blank") : false);
+        (strlen($jsonData->username) > 255 ? $response->addMessage("Email cannot be greater than 255 characters") : false);
         (strlen($jsonData->password) < 1 ? $response->addMessage("Password cannot be blank") : false);
         (strlen($jsonData->password) > 255 ? $response->addMessage("Password cannot be greater than 255 characters") : false);
         $response->send();
@@ -210,34 +227,33 @@ if(array_key_exists("sessionid", $_GET)) {
     }
 
     try {
-
-        $username = $jsonData->username;
+        //assign the content username and password
+        $email = $jsonData->email;
         $password = $jsonData->password;
-
-        $query = $dB->prepare('select id, first_name, last_name, phone, email from volunteers where email = :email');
-        $query->bindParam(':username', $username, PDO::PARAM_STR);
+        // select the volunteer
+        $query = $dB->prepare('select id, email, password from volunteers where email = :email');
+        $query->bindParam(':username', $email, PDO::PARAM_STR);
         $query->execute();
 
         $rowCount = $query->rowCount();
-
+        // if the volunteer is not found
         if($rowCount === 0) {
             $response = new Response();
             $response->setHttpStatusCode(401);
             $response->setSuccess(false);
-            $response->addMessage("email or password is incorrect");
+            $response->addMessage("email or password is incorrect");  // generic error message
             $response->send();
             exit; 
         }
 
         $row = $query->fetch(PDO::FETCH_ASSOC);
-
+        // assign the vonteer details
         $returned_id = $row['id'];
-        $returned_fullname = $row['first_name'] . $row['last_name'];
         $returned_username = $row['email'];
         $returned_password = $row['password'];
         
 
-
+        // check the provided password matches the volunteers password
         if(!password_verify($password, $returned_password)) {
             $response = new Response();
             $response->setHttpStatusCode(401);
@@ -247,6 +263,7 @@ if(array_key_exists("sessionid", $_GET)) {
             $response->send();
             exit; 
         }
+    // general database error
     } catch(PDOException $e) {
         $response = new Response();
         $response->setHttpStatusCode(500);
@@ -255,19 +272,17 @@ if(array_key_exists("sessionid", $_GET)) {
         $response->send();
         exit; 
     }
-
+        // create the access and refresh token
         $accesstoken = base64_encode(bin2Hex(openssl_random_pseudo_bytes(24)).time());
         $refreshtoken = base64_encode(bin2Hex(openssl_random_pseudo_bytes(24)).time());
 
+        //set the expiry times for the access and refresh tokens
         $access_token_expiry_seconds = 7200; //2 hours
         $refresh_token_expiry_seconds = 1209600; // 14 days
 
         try {
-            $dB->beginTransaction();
-            $query = $dB->prepare('update tblusers set loginattempts = 0 where id = :id');
-            $query->bindParam(':id', $returned_id, PDO::PARAM_INT);
-            $query->execute();
-
+            
+            //set up the session for the volunteer
             $query = $dB->prepare('insert into tblsessions (userid, accesstoken, accesstokenexpiry, refreshtoken, refreshtokenexpiry) values (:userid, :accesstoken, date_add(NOW(), INTERVAL :accesstokenexpiryseconds SECOND), :refreshtoken, date_add(NOW(), INTERVAL :refreshtokenexpiryseconds SECOND))');
             $query->bindParam(':userid', $returned_id, PDO::PARAM_INT);
             $query->bindParam(':accesstoken', $accesstoken, PDO::PARAM_STR);
@@ -286,14 +301,14 @@ if(array_key_exists("sessionid", $_GET)) {
             $returnData['access_token_expires_in'] = $access_token_expiry_seconds;
             $returnData['refresh_token'] = $refreshtoken;
             $returnData['refresh_token_expires_in'] = $refresh_token_expiry_seconds;
-
+            // login the user and set confirmation response
             $response = new Response();
             $response->setHttpStatusCode(201);
             $response->setSuccess(true);
             $response->setData($returnData);
             $response->send();
             exit; 
-            
+        // general databse error
         } catch(PDOException $e) {
             $dB->rollBack();
             $response = new Response();
@@ -303,7 +318,7 @@ if(array_key_exists("sessionid", $_GET)) {
             $response->send();
             exit; 
         }
-
+// if everything fails
 } else {
     $response = new Response();
     $response->setHttpStatusCode(404);
--- public/data/controller/volunteer.php
+++ public/data/controller/volunteer.php
@@ -2,6 +2,15 @@
 require_once('db.php');
 require_once('../model/Response.php');
 
+/*
+Sign up a volunteer and add them to the database
+user created with post Json data consisting of
+first_name, last_name, email, phone, password
+All fieds are required and fields validated before adding to the database
+Response sent to client using the response model and json data listing errors or success
+*/
+
+// database connection
 try{
     $dB = DB::connectDB();
 } catch(PDOException $e) {
@@ -13,6 +22,8 @@ try{
     $response->send();
     exit;
 }
+
+// sign up a volunteer with post content
 if($_SERVER['REQUEST_METHOD'] !=='POST') {
     $response = new Response();
     $response->setHttpStatusCode(405);
@@ -21,7 +32,7 @@ if($_SERVER['REQUEST_METHOD'] !=='POST') {
     $response->send();
     exit;
 }
-
+// check content header is set to json
 if($_SERVER['CONTENT_TYPE'] !== 'application/json') {
     $response = new Response();
     $response->setHttpStatusCode(400);
@@ -30,9 +41,9 @@ if($_SERVER['CONTENT_TYPE'] !== 'application/json') {
     $response->send();
     exit;
 }
-
+//assign the content
 $rowPostData = file_get_contents('php://input');
-
+// check content is valid json
 if(!$jsonData = json_decode($rowPostData)) {
     $response = new Response();
     $response->setHttpStatusCode(400);
@@ -41,39 +52,52 @@ if(!$jsonData = json_decode($rowPostData)) {
     $response->send();
     exit;
 }
-
-if(!isset($jsonData->fullname)|| !isset($jsonData->username) || !isset($jsonData->password)) {
+// some information is not supplied
+if(!isset($jsonData->first_name) || !isset($jsonData->last_name) || !isset($jsonData->email) || !isset($jsonData->phone) || !isset($jsonData->password)) {
     $response = new Response();
     $response->setHttpStatusCode(400);
     $response->setSuccess(false);
-    (!isset($jsonData->fullname) ? $response->addMessage("Fullname not supplied") : false);
-    (!isset($jsonData->username) ? $response->addMessage("Username not supplied") : false);
+    (!isset($jsonData->first_name) ? $response->addMessage("first name not supplied") : false);
+    (!isset($jsonData->last_name) ? $response->addMessage("last name not supplied") : false);
+    (!isset($jsonData->email) ? $response->addMessage("email not supplied") : false);
+    (!isset($jsonData->phone) ? $response->addMessage("phone number not supplied") : false);
     (!isset($jsonData->password) ? $response->addMessage("Password not supplied") : false);
     $response->send();
     exit;
 }
 
-if(strlen($jsonData->fullname) < 1 || strlen($jsonData->fullname) > 255 || strlen($jsonData->username) < 1 || strlen($jsonData->username) > 255 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 255) {
+// validate the data is not blank and does notexceed the character limit
+if(strlen($jsonData->first_name) < 1 || strlen($jsonData->first_name) > 40 ||  strlen($jsonData->last_name) < 1 || strlen($jsonData->last_name) > 40 || !filter_var($jsonData->email, FILTER_VALIDATE_EMAIL) || strlen($jsonData->phone) < 1 || strlen($jsonData->phone) > 16 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 255) {
     $response = new Response();
     $response->setHttpStatusCode(400);
     $response->setSuccess(false);
-    (strlen($jsonData->fullname) < 1 ? $response->addMessage("Fullname cannot be blank") : false);
-    (strlen($jsonData->fullname) > 255 ? $response->addMessage("Fullname cannot be greater than 255 characters") : false);
-    (strlen($jsonData->username) < 1 ? $response->addMessage("Username cannot be blank") : false);
-    (strlen($jsonData->username) > 255 ? $response->addMessage("Username cannot be greater than 255 characters") : false);
+    (strlen($jsonData->first_name) < 1 ? $response->addMessage("First name cannot be blank") : false);
+    (strlen($jsonData->first_name) > 255 ? $response->addMessage("First name cannot be greater than 40 characters") : false);
+    (strlen($jsonData->last_name) < 1 ? $response->addMessage("Last name cannot be blank") : false);
+    (strlen($jsonData->last_name) > 255 ? $response->addMessage("Last name cannot be greater than 40 characters") : false);
+    (!filter_var($jsonData->email, FILTER_VALIDATE_EMAIL)?$response->addMessage("The email is not a valid email") : false);
+    (strlen($jsonData->phone) < 1 ? $response->addMessage("Phone cannot be blank") : false);
+    (strlen($jsonData->phone) > 255 ? $response->addMessage("Phone cannot be greater than 16 digits") : false);
     (strlen($jsonData->password) < 1 ? $response->addMessage("Password cannot be blank") : false);
     (strlen($jsonData->password) > 255 ? $response->addMessage("Password cannot be greater than 255 characters") : false);
     $response->send();
     exit;
 }
 
-$fullname = trim($jsonData->fullname);
-$username = trim($jsonData->username);
+
+// trim white space or other characters and check the email is a valid email and assign them
+$first_name = trim($jsonData->first_name);
+$last_name = trim($jsonData->last_name);
+$email = $jsonData->email;
+$phone = trim($jsonData->phone);
+// password could have any character
 $password = $jsonData->password;
 
 try {
-    $query = $dB->prepare('select id from tblusers where username = :username');
-    $query->bindParam(':username', $username, PDO::PARAM_STR);
+
+    //Check the volunteer doesn't already exist
+    $query = $dB->prepare('select id from tblusers where email = :email');
+    $query->bindParam(':username', $email, PDO::PARAM_STR);
     $query->execute();
 
     $rowCount = $query->rowCount();
@@ -82,21 +106,23 @@ try {
         $response = new Response();
         $response->setHttpStatusCode(409); //conflict error
         $response->setSuccess(false);
-        $response->addMessage("Username already exists - please try again");
+        $response->addMessage("Volunteer already exists with that email- please try again");
         $response->send();
         exit;
     }
-
+    // secure thepassword to stroe in the database
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    $query = $dB->prepare('insert into tblusers (fullname, username, password) values(:fullname, :username, :password)');
-    $query->bindParam(':fullname', $fullname, PDO::PARAM_STR);
-    $query->bindParam(':username', $username, PDO::PARAM_STR);
+    // add the volunteer
+    $query = $dB->prepare('insert into tblusers (first_name, last_name, email, phone, password) values(:first_name, :last_name, :email, :phone , :password)');
+    $query->bindParam(':first_name', $first_name, PDO::PARAM_STR);
+    $query->bindParam(':last_name', $last_name, PDO::PARAM_STR);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':phone', $phone, PDO::PARAM_STR);
     $query->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
     $query->execute();
 
     $rowCount = $query->rowCount();
-
+    // if there was a problem
     if($rowCount === 0) {
         $response = new Response();
         $response->setHttpStatusCode(500); 
@@ -105,12 +131,13 @@ try {
         $response->send();
         exit;
     }
-
+    // return a confirmation response
     $lastUserID = $dB->lastInsertID();
     $returnData = array();
     $returnData['user_id'] = $lastUserID;
-    $returnData['fullname'] = $fullname;
-    $returnData['username'] = $username;
+    $returnData['first_name'] = $first_name;
+    $returnData['last_name'] = $last_name;
+    $returnData['email'] = $email;
 
     $response = new Response();
     $response->setHttpStatusCode(201); 
@@ -120,7 +147,7 @@ try {
     $response->send();
     exit;
 
-
+// general database error
 }catch(PDOException $e){
     error_log("Database query error - ".$e, 0);
     $response = new Response();
